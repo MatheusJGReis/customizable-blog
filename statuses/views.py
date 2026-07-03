22ff2b4c126749f73dd23da29545aeba3f05c156
Matheus Reis <matheus.jgreis06@gmail.com>
2026-07-03 15:37:12 -0300

    style: added type annotations on statuses/views.py

--- statuses/views.py
+++ statuses/views.py
@@ -7,8 +7,8 @@ from .models import Status
 
 def list(request: HttpRequest) -> HttpResponse:
     statuses: models.BaseManager[Status] = Status.objects.all().order_by('-created_at')
-    paginator = Paginator(statuses, 12)
-    page_obj = paginator.get_page(request.GET.get('page'))
+    paginator: Paginator = Paginator(statuses, 12)
+    page_obj: Page = paginator.get_page(request.GET.get('page'))
 
     return render(request, 'statuses/list.html', {
         'statuses': page_obj,
